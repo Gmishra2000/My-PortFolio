0fe974db55cc70916f6096063d26d1914c951351
Gmishra2000 <mantumisara485@gmail.com>
2021-07-30 22:20:44 +0530

    added till topbar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import PersonIcon from '@material-ui/icons/Person';
+import MailIcon from '@material-ui/icons/Mail';
 import "./topbar.scss"
-export default function Topbar() {
+export default function Topbar({ menuOpen, setMenuOpen }) {
     return (
-        <div className="topbar">
-            
+        <div className={"topbar " + (menuOpen && "active")} >
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#works" className="logo">genius.</a>
+                    <div className="itemContainer">
+                        <PersonIcon className="icon"/>
+                        <span>+91 83692 91890</span>
+                    </div>
+                    <div className="itemContainer">
+                        <MailIcon className="icon"/>
+                        <span>mantumisara485@gmail.com</span>
+                    </div>
+                </div>
+                
+                <div className="right">
+                    <div className="hamburger" onClick= {() => setMenuOpen(!menuOpen)}>
+                        <span className="line1"></span>
+                        <span className="line2"></span>
+                        <span className="line3"></span>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
