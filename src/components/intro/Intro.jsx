bdb773a4d85ae491e3dd24e6fc6c168886eb244e
Gmishra2000 <mantumisara485@gmail.com>
2021-10-16 16:37:28 +0530

    In new Branch

---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,29 @@
+import './intro.css'
+
+const Intro = () => {
+    return (
+        <div className='i'>
+            <div className="i-left">
+                <div className="i-left-wrapper">
+                    <h2 className="i-intro">Hello, My name is</h2>
+                    <h1 className="i-name">Chandan Mishra</h1>
+                    <div className="i-title">
+                        <div className="i-title-wrapper">
+                            <div className="i-title-item">Web Developer</div>
+                            <div className="i-title-item">UI/UX Designer</div>
+                            <div className="i-title-item">Front End Developer</div>
+                            <div className="i-title-item">Undergraduate</div>
+                            <div className="i-title-item">Computer Science Student</div>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="i-right">
+            right
+            </div>
+        </div>
+    )
+}
+
+export default Intro
