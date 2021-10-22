e7892aa249036cec84555c2f7778d8b323acbf90
Gmishra2000 <mantumisara485@gmail.com>
2021-10-22 13:48:28 +0530

    projectList

---
+++ src/components/productlist/ProductList.jsx
@@ -0,0 +1,32 @@
+import "./productlist.css";
+import Product from "../product/Product";
+import { products } from "../../data"
+
+const ProductList = () => {
+    return (
+        <div>
+            <div className="pl">
+                <div className="pl-texts">
+                    <h1 className="pl-title">Create & innovate. It's Lama</h1>
+                    <p className="pl-desc">
+                        Lama is a creative portfolio that your work has been waiting for.
+                        Beautiful homes, stunning portfolio styles & a whole lot more awaits
+                        inside.
+                    </p>
+                </div>
+                <div className="pl-list">
+                    
+                    {products.map((item) => (
+                        <Product key={item.id} img={item.img} link={item.link} />
+           ))}
+                </div>
+            </div>
+           
+        </div>
+        
+    )
+}
+
+export default ProductList
+
+
---
+++ src/data.js
@@ -0,0 +1,33 @@
+export const products = [{
+        id: 1,
+        img: "assets/image/placement-prep.png",
+        link: "http://lama.dev",
+    },
+    {
+        id: 2,
+        img: "assets/image/placement-prep.png",
+        link: "http://lama.dev",
+    },
+    {
+        id: 3,
+        img: "assets/image/placement-prep.png",
+        link: "http://lama.dev",
+    },
+    {
+        id: 4,
+        img: "assets/image/placement-prep.png",
+        link: "http://lama.dev",
+    },
+    {
+        id: 5,
+        img: "assets/image/placement-prep.png",
+        link: "http://lama.dev",
+    },
+    {
+        id: 6,
+        img: "assets/image/placement-prep.png",
+        link: "http://lama.dev",
+    },
+];
+
+// https://i.ibb.co/6cKRBfN/3.jpg
